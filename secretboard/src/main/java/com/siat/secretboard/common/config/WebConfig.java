e68e6f3367dd3bf2934ac3cabace1ac2675d5de7
정지훈 <mudokja@gmail.com>
2025-05-13 17:55:46 +0900

    fix: cors 설정 방식 수정

---
+++ secretboard/src/main/java/com/siat/secretboard/common/config/WebConfig.java
@@ -0,0 +1,25 @@
+package com.siat.secretboard.common.config;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Value("#{'${secretboard.config.cors.allowOrigns}'.trim().split(',')}")
+    private List<String> allowedOrigins;
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowCredentials(true)
+                .allowedOrigins(allowedOrigins.toArray(String[]::new))
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization");
+    }
+}
