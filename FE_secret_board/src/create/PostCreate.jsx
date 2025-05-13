921e3f64d45e7feddf2774fa59667036afa0346a
Byung-yun <quddbstmvls@gmail.com>
2025-05-13 14:09:17 +0900

    feat - PostCreate.jsx - 등록 UI 요소 구현

--- FE_secret_board/src/create/PostCreate.jsx
+++ FE_secret_board/src/create/PostCreate.jsx
@@ -1,31 +1,38 @@
 import React from "react";
 
 function PostCreate() {
-  return (
-    <div>
-        <form class="mx-3 mt-3">
-            <div class="mx-1 d-flex justify-content-between">
-                <label class="form-label">R!</label>
-                <label class="form-label">YYYY-MM-DD</label>
-            </div>
-            <div class="mb-3">
-                <div class="form-floating">
-                    <input type="text" placeholder="Title" class="form-control" id="floatingTitle"/>
-                    <label for="floatingTitle">Title</label>
+    const [title, setTitle] = React.useState("");
+    const [content, setContent] = React.useState("");
+
+    const submitHandler = (e) => {
+        console.log("submitHandler");
+        e.preventDefault();
+    }
+    return (
+        <div>
+            <form class="mx-3 mt-3">
+                <div class="mx-1 d-flex justify-content-between">
+                    <label class="form-label">R!</label>
+                    <label class="form-label">YYYY-MM-DD</label>
                 </div>
-            </div>
-            <div class="mb-3">
-                <div class="form-floating">
-                    <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style={{height: "300px"}}></textarea>
-                    <label for="floatingTextarea">Content</label>
+                <div class="mb-3">
+                    <div class="form-floating">
+                        <input type="text" placeholder="Title" class="form-control" id="floatingTitle" />
+                        <label for="floatingTitle">Title</label>
+                    </div>
                 </div>
-            </div>
-            <div style={{textAlign: "right"}}>
-                <button type="submit" class="btn btn-primary">등록</button>
-            </div>
-        </form>
-    </div>
-  );
+                <div class="mb-3">
+                    <div class="form-floating">
+                        <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea" style={{ height: "300px" }}></textarea>
+                        <label for="floatingTextarea">Content</label>
+                    </div>
+                </div>
+                <div style={{ textAlign: "right" }}>
+                    <button type="submit" class="btn btn-primary" onClick={submitHandler}>등록</button>
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default PostCreate;
