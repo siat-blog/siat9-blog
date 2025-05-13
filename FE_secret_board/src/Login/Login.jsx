33414af72ee3545eb20448aab4332b01541b8199
young <eycho93@naver.com>
2025-05-13 12:27:55 +0900

    style - login.jsx - 스타일 변경

--- FE_secret_board/src/Login/Login.jsx
+++ FE_secret_board/src/Login/Login.jsx
@@ -1,24 +1,34 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 
 function Login() {
   return (
-    <div>
-      <h1>Login</h1>
-      <form>
-        <label>
-          Username:
-          <input type="text" name="username" />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" name="password" />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-      </form>
+    <div className="container mt-5">
+      <div className="row justify-content-center">
+        <div className="col-md-6"> {/* 너비 조절 */}
+
+          <h1 className="text-center">Login</h1>
+
+          <form>
+            <div className="mb-3">
+              <label>Id:</label>
+              <input type="text" name="Id" placeholder="Id" className="form-control" />
+            </div>
+
+            <div className="mb-3">
+              <label>Password:</label>
+              <input type="password" name="password" placeholder="Password" className="form-control" />
+            </div>
+
+            <div className="d-grid gap-2">
+              <Button variant="primary" size="lg" type="submit">Login</Button>
+              <Button variant="primary" size="lg" type="button">SignUp</Button>
+            </div>
+          </form>
+
+        </div>
+      </div>
     </div>
   );
 }
-
 export default Login;
