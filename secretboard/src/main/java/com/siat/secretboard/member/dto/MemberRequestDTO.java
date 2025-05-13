aa885833e7043007811490c386ff5e2371301eee
davJ-star <jshsy0130@gmail.com>
2025-05-13 15:52:18 +0900

    feat: add MemberRequestDTO and initial structure for MemberResponseDTO

--- secretboard/src/main/java/com/siat/secretboard/member/dto/MemberRequestDTO.java
+++ secretboard/src/main/java/com/siat/secretboard/member/dto/MemberRequestDTO.java
@@ -1,5 +1,13 @@
 package com.siat.secretboard.member.dto;
 
+import lombok.Getter;
+
+
+@Getter
 public class MemberRequestDTO {
+    private String memberNickname;
+    private String memberId;
+    private String memberPassword;
+    private Long boardIdx;
     
 }
---
+++ secretboard/src/main/java/com/siat/secretboard/member/dto/MemberResponseDTO.java
@@ -0,0 +1,13 @@
+package com.siat.secretboard.member.dto;
+
+import lombok.Getter;
+
+
+// @Getter
+public class MemberResponseDTO {
+    // private String memberNickname;
+    // private String memberId;
+    // private String memberPassword;
+    // private Long boardIdx;
+    
+}
