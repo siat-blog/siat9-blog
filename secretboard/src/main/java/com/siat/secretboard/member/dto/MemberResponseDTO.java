5682523e0f117d8bca3c598b248a153192c12877
davJ-star <jshsy0130@gmail.com>
2025-05-14 13:46:20 +0900

    feat: update MemberRequestDTO and MemberResponseDTO for member ID handling and refine MemberServiceImpl implementation

--- secretboard/src/main/java/com/siat/secretboard/member/dto/MemberResponseDTO.java
+++ secretboard/src/main/java/com/siat/secretboard/member/dto/MemberResponseDTO.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 @Builder
 public class MemberResponseDTO {
     // 수정해야함.
+    private String memberId; // memberIdx랑 구분된 상태이며, 어떤 것을 이용할지 논의 해봐야함.
     private String memberNickname;
-    private String memberId;
     private String memberPassword;
     private String boardType; // 일단 추가. -> 수정해야함.
     
