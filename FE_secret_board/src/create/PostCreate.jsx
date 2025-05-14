f00a8dbec13fd5a219d2b90da333a65c6bf9e6cb
Byung-yun <quddbstmvls@gmail.com>
2025-05-14 15:42:39 +0900

    fix - PostCreate.jsx - required 속성 추가

--- FE_secret_board/src/create/PostCreate.jsx
+++ FE_secret_board/src/create/PostCreate.jsx
@@ -1,15 +1,10 @@
 import React, { use } from "react";
 import api from "../api/axios";
 import { useNavigate } from "react-router-dom";
-import { createContext } from 'react';
 
 function PostCreate() {
     const [title, setTitle] = React.useState("");
     const [content, setContent] = React.useState("");
-    const [nickname, setNickname] = React.useState("null");
-    const [date, setDate] = React.useState("YYYY-MM-DD");
-    const id = "id";
-
     const moveUrl = useNavigate();
 
     const submitHandler = async (e) => {
@@ -17,7 +12,7 @@ function PostCreate() {
         console.log("PostCreate > submitHandler");
 
         if (window.confirm("게시물을 등록하시겠습니까??")) {
-            await api.post(`/api/post/${id}`, {
+            await api.post(`/api/post/`, {
                 title: title,
                 content: content,
             });
@@ -29,16 +24,13 @@ function PostCreate() {
     return (
         <div>
             <form className="mx-3 mt-3">
-                <div className="d-flex justify-content-between">
-                    <p className="mx-1">{nickname}</p>
-                    <p className="mx-1">{date}</p>
-                </div>
                 <div className="mb-3">
                     <input
                         type="text"
                         placeholder="제목"
                         className="form-control"
                         value={title}
+                        required="text"
                         onChange={(e) => setTitle(e.target.value)}
                     />
                 </div>
@@ -48,6 +40,7 @@ function PostCreate() {
                         placeholder="내용"
                         style={{ height: "300px" }}
                         value={content}
+                        required="text"
                         onChange={(e) => setContent(e.target.value)}
                     />
                 </div>
