87313cb0ad6827df46f4eaeb65de55c24b4f9c97
young <eycho93@naver.com>
2025-05-14 10:52:37 +0900

    feat: merge latest SignUp branch from origin

--- FE_secret_board/src/App.js
+++ FE_secret_board/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { BrowserRouter , Route, Routes} from "react-router-dom";
 import './App.css';
+
+import Login from "./Login/Login"
+import SignUp from "./SignUp/SignUp"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* <h2>openapi 위한 라우터 연습</h2> */}
+      <Routes>
+        {/* 라우터당 컴포넌트 연결 */}
+        <Route path="/"             element={<Login />}></Route>  
+        <Route path="/SignUp"         element={<SignUp />}></Route>  
+      </Routes>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
