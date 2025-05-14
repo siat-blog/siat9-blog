6192a151bdbc6bce40fa5504b37208dd731424a9
young <eycho93@naver.com>
2025-05-14 10:59:52 +0900

    feat - SignUp.jax - 인덱스설정

--- FE_secret_board/src/index.js
+++ FE_secret_board/src/index.js
@@ -4,11 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import SignUp from './SignUp/SignUp';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <SignUp />
   </React.StrictMode>
 );
 
