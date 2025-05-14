78ab3868f6f8e87ab237a3118a761003599c39fa
cmion <yeomyeongforbuisness@gmail.com>
2025-05-14 09:51:52 +0900

    fix - 브랜치 실수

--- FE_secret_board/src/index.js
+++ FE_secret_board/src/index.js
@@ -14,7 +14,7 @@ import PostRead from './postread/PostRead';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <PostCreate />
+    <App />
   </React.StrictMode>
 );
 
