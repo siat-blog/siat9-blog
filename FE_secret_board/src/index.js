ebc4d9e18e3d0cf92cf0bdce4394fd3e72678ce7
young <eycho93@naver.com>
2025-05-14 17:13:40 +0900

    feat - index.js - App으로 설정

--- FE_secret_board/src/index.js
+++ FE_secret_board/src/index.js
@@ -1,28 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Login from './Login/Login';
+// import Login from './Login/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserContextProvider from './context/UserContextProvider';
-
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <UserContextProvider>
-      <Login /> 
-      </UserContextProvider>
+      <App />
+    </UserContextProvider>
+    
   </React.StrictMode>
 );
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <UserContextProvider>
+//       <Login /> 
+//       </UserContextProvider>
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
