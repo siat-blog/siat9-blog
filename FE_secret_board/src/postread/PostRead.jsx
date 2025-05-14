36e583d510fb8074be50c7649bbb766d0f31f8f1
Byung-yun <quddbstmvls@gmail.com>
2025-05-14 16:16:52 +0900

    feat - PageUpdate.jsx - put요청을 위한 페이지 추가

--- FE_secret_board/src/postread/PostRead.jsx
+++ FE_secret_board/src/postread/PostRead.jsx
@@ -1,42 +1,54 @@
 import React from "react";
+import api from "../api/axios";
 
 function PostRead(props) {
-    const [title, setTitle] = React.useState("");
-    const [content, setContent] = React.useState("");
-
-    const deleteHandler = (e) => {
+    const data  = useLocation().state;
+    const deleteHandler = async (e) => {
         e.preventDefault();
         console.log("PostRead > deleteHandler");
 
         if (window.confirm("게시물을 삭제하시겠습니까??")) {
-            // api 요청
+            await api.delete(`api/post/`)
+                .then((res) => {
+                    console.log(res);
+                    if (res.status === 200) {
+                        alert("게시물이 삭제되었습니다.");
+                        moveUrl("/PostList");
+                    } else {
+                        alert("게시물 삭제에 실패했습니다.");
+                    }
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
         } else {
         }
     }
-    const updateHandler = (e) => {
+    const moveToPostUpdate = (e) => {
         e.preventDefault();
-        console.log("PostRead > updateHandler");
+        moveUrl('/PostUpdate', { state: { data: data } });
+
     }
 
     return (
         <div>
             <form class="mx-3 mt-3">
                 <div class="d-flex justify-content-between">
-                    <label class="form-label">닉네임</label>
-                    <label class="form-label">YYYY-MM-DD</label>
+                    <p>{data.nickName}</p>
+                    <p>{data.date}</p>
                 </div>
                 <div class="mb-3">
                     <div class="form-floating">
-                        <h4 class="">제목</h4>
+                        <h4 class="">{data.title}</h4>
                     </div>
                 </div>
                 <div class="mb-3">
                     <div class="form-floating">
-                    <p style={{ height: "300px", overflowY: "scroll" }}>내용</p>
+                    <p style={{ height: "300px", overflowY: "scroll" }}>{data.content}</p>
                     </div>
                 </div>
                 <div style={{ textAlign: "right" }}>
-                    <button type="submit" class="btn btn-primary" onClick={updateHandler}>수정</button>
+                    <button type="submit" class="btn btn-primary" onClick={moveToPostUpdate}>수정</button>
                     <button type="submit" class="btn btn-primary mx-2" onClick={deleteHandler}>삭제</button>
                 </div>
             </form>
