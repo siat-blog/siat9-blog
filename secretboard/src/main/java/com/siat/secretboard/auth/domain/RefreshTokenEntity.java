57e6412d3e1f6afb5c87b4684cf45ec2dc1b4000
Dream-Choi <cmb02236@gmail.com>
2025-05-14 12:01:53 +0900

    fix: 오류 수정

--- secretboard/src/main/java/com/siat/secretboard/auth/domain/RefreshTokenEntity.java
+++ secretboard/src/main/java/com/siat/secretboard/auth/domain/RefreshTokenEntity.java
@@ -1,8 +1,10 @@
 package com.siat.secretboard.auth.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
 public class RefreshTokenEntity {
-    
+    @Id
+    private String refreshToken;
 }
